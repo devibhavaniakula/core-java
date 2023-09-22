dccca11a917072b3b216b5ff4cf9dbc0327ea20f
acer <acer@10.0.0.102>
2023-09-22 14:26:43 +0530

    Second Commit

---
+++ project1/src/com/tns/prog/EncapsuationDemo.java
@@ -0,0 +1,15 @@
+package com.tns.prog;
+
+public class EncapsuationDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Encapsulation ob=new Encapsulation(); //creation of object
+ob.serialno=02;
+ob.name="Devi";
+ob.age=21;
+ob.show();
+
+	}
+
+}
---
+++ project1/src/com/tns/prog/Encapsulation.java
@@ -0,0 +1,12 @@
+package com.tns.prog;
+//program to demonstrate encapsulation in java
+public class Encapsulation {
+//data members
+	public int serialno;
+	public String name;
+	public int age;
+	//member function
+	public void show() {
+	System.out.println("serial no"+serialno+"\t"+"name"+name+"\t"+"age"+age);
+	}
+	}
