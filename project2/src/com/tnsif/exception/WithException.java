442b6dab88deb4ecdc14a351c4c28234bec4b76c
acer <acer@10.0.0.148>
2023-10-12 15:27:18 +0530

    eighth commit

---
+++ project2/src/com/tnsif/exception/WithException.java
@@ -0,0 +1,27 @@
+package com.tnsif.exception;
+
+public class WithException {
+	public static void divide() {
+		int a=6,b=0,c;
+		try
+		{
+			c=a/b;
+		}
+		catch(ArithmeticException e) {
+			System.out.println("exception caught"+e.getMessage());
+			
+		}
+		catch(ArrayIndexOutOfBoundsException e) {
+			System.out.println("exception caught"+e.getMessage());
+			
+		}
+		catch(Exception e) {
+			System.out.println("exception caught"+e.getMessage());
+			
+		}
+		finally {
+			System.out.println("this will be executed");
+		}
+	}
+
+}
