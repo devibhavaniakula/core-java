1fbeb039342253a1ced3adf2743619210f703bc2
acer <acer@192.168.1.53>
2023-10-31 14:58:33 +0530

    ele commit

---
+++ project2/src/tnsif/fivemtl/Fivemul.java
@@ -0,0 +1,15 @@
+package tnsif.fivemtl;
+
+	public class Fivemul {
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			int a,five_m;//1
+			a=22;//2 static initialization
+			five_m=5*a;//dynamic
+			System.out.println("The fifth multiple of 22 is" + five_m);
+
+		}
+	}
+
+
