fd7fda1c703269e287c049051b7067309b9c4874
acer <acer@192.168.1.53>
2023-11-02 11:46:25 +0530

    commit

---
+++ project2/src/wrapperclass/Demo.java
@@ -0,0 +1,16 @@
+package wrapperclass;
+
+public class Demo {
+
+	
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+	int x=15;
+	Integer i=new Integer(x);
+	System.out.println("x is object of integer type"+i);
+	int u=i.intValue();
+	System.out.println("u is primitive intger"+u);
+		}
+
+	}
