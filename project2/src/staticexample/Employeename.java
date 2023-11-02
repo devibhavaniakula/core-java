d1e56a71135a1c1ba69a355ed479c360901464ca
acer <acer@192.168.1.53>
2023-11-02 11:14:24 +0530

    commit

---
+++ project2/src/staticexample/Employeename.java
@@ -0,0 +1,13 @@
+package staticexample;
+
+public class Employeename {
+
+	
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+	    Employee ob=new Employee("suma",123);
+	    System.out.println(ob);
+	    }
+
+	}
